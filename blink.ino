6a416407d1be5a3e0a25d1566b423beb2b28f140
mintywill <35755814+mintywill@users.noreply.github.com>
2018-03-25 15:55:57 -0700

    Create blink.ino

---
+++ blink.ino
@@ -0,0 +1,36 @@
+int sensorValue = 0;  
+int sensorPin = A0; 
+
+void setup() {
+  pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(11, OUTPUT);
+  
+  Serial.begin(9600);
+}
+
+void loop() {
+  sensorValue = analogRead(sensorPin);
+  if (sensorValue == 1023) {
+    digitalWrite(13, LOW);
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    sensorValue = analogRead(sensorPin);
+    
+  } else if (sensorValue <= 250) {
+    digitalWrite(13, HIGH);
+    sensorValue = analogRead(sensorPin);
+  } else if (sensorValue <= 510) {
+    digitalWrite(12, HIGH);
+    sensorValue = analogRead(sensorPin);
+  } else if (sensorValue <= 760) {
+    digitalWrite(11, HIGH); 
+    sensorValue = analogRead(sensorPin);
+  }
+  
+  Serial.println(sensorValue);  
+  digitalWrite(13, HIGH);
+  delay(sensorValue);
+  digitalWrite(13, LOW);
+  delay(sensorValue);
+}
