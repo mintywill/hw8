9ea63aca6ba4766ef2dae91467194d93b97350d5
mintywill <35755814+mintywill@users.noreply.github.com>
2018-03-25 14:54:35 -0700

    Create doctor.ino

---
+++ doctor.ino
@@ -0,0 +1,23 @@
+int sensorValue = 0;
+int sensorPin = A0;
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 100;    // how many points to fade the LED by
+
+void setup() {
+  pinMode(13, OUTPUT);
+}
+
+void loop() {
+  // set the brightness of pin 9:
+  analogWrite(13, brightness);
+
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 10 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  delay(50);               
+  digitalWrite(13, LOW);        
+  delay(100);
+}
