326f95fdee5b7c56df90c073d0cc60e8e5cb4a45
mintywill <35755814+mintywill@users.noreply.github.com>
2018-03-25 14:40:32 -0700

    Create overdue.ino

---
+++ overdue.ino
@@ -0,0 +1,24 @@
+int sensorValue = 0;
+int sensorPin = A0;
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 10;    // how many points to fade the LED by
+
+// the setup routine runs once when you press reset:
+void setup() {
+  // declare pin 9 to be an output:
+  pinMode(13, OUTPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // set the brightness of pin 9:
+  analogWrite(13, brightness);
+
+  // change the brightness for next time through the loop:
+  brightness = brightness + fadeAmount;
+
+  // reverse the direction of the fading at the ends of the fade:
+  if (brightness <= 0 || brightness >= 255) {
+    fadeAmount = -fadeAmount;
+  }
+  delay(300);
